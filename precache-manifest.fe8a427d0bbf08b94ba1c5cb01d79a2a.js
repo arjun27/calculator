40f547dd822d15ae1dd33e55a38115dc04df91f4
Arjun Attam <arjunattam@gmail.com>
2019-12-13 12:58:16 -0800

    Updates

---
+++ precache-manifest.fe8a427d0bbf08b94ba1c5cb01d79a2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1bf681b941c5202f54f94becf7a93e1",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "f9b7214968b4404c0b80",
+    "url": "/calculator/static/css/main.86f680a8.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "f9b7214968b4404c0b80",
+    "url": "/calculator/static/js/main.fcdb266f.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
